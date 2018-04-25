1e6e5ad6d6f200e512f487f32dd3a7aadd879f38
Guillaume <groult_guigui@hotmail.com>
2018-04-25 03:20:30 +0200

    Ajout de commentaires

--- preview.js
+++ preview.js
@@ -1,14 +1,16 @@
 var preview = document.getElementById('preview');
 
+
 function ClicDisplay(src, alt){
+	/* Modifications des attributs des enfants de l'id preview */
 	preview.firstChild.src=src;
 	preview.firstChild.alt=alt;
 
-	preview.lastChild.innerHTML= alt;
+	preview.lastChild.innerHTML= alt; //modification du paragraphe de l'id preview
 	
-	preview.style.display = 'block';
+	preview.style.display = 'block'; //On rend l'element visible en modifiant la propriété display
 }
 
 function ClicClear(){
-	preview.style.display = 'none';
+	preview.style.display = 'none'; //On fait disparaître l'id preview 
 }
